9c3258fd838c902998bd256001aa360669d93d7a
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-04-18 00:09:03 +0200

    add: home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  userName: string = '';
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    const userMail = this.auth.getStatus();
+    const userListString = localStorage.getItem('userList');
+    const userList = !!userListString ? JSON.parse(userListString) : [];
+    userList.forEach((u: { email: string; name: string }) => {
+      if (u.email === userMail) {
+        this.userName = u.name;
+        return;
+      }
+    });
+  }
+}
