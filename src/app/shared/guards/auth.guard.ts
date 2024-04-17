8366136c0eb1a13cfac729d11c11c6f5e95f1cc0
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-04-17 23:45:47 +0200

    fix do fix: services, interfaces etc -> /shared

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  if (auth.getStatus() == 'none') {
+    router.navigate(['login']);
+    return false;
+  } else {
+    return true;
+  }
+};
---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {
+    this.getStatus();
+  }
+
+  getStatus(): string {
+    const auth = localStorage.getItem('auth');
+    if (!auth) {
+      this.logout();
+      return 'none';
+    } else {
+      return auth;
+    }
+  }
+
+  login(email: string, password: string): string {
+    const userListString = localStorage.getItem('userList');
+    const userList = !!userListString ? JSON.parse(userListString) : [];
+    const user = userList.find(
+      (u: { email: string; password: string }) =>
+        u.email === email && u.password === password
+    );
+
+    if (user) {
+      localStorage.setItem('auth', user.email);
+      return 'success';
+    } else {
+      this.logout();
+      return 'invalid_credentials';
+    }
+  }
+
+  logout() {
+    localStorage.setItem('auth', 'none');
+  }
+}
