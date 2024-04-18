d8b36b0eaf8daf5e020c084c941d36b009fed14b
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-04-18 06:04:50 +0200

    finish: styles header and sidebar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router, private auth: AuthService) {}
+
+  logout() {
+    console.log('sair da sessão!');
+    this.auth.logout();
+    this.router.navigate(['login']);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent],
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ButtonModule],
   exports: [HeaderComponent, SidebarComponent],
 })
 export class SharedModule {}
